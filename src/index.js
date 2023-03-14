3b47848188cfc8f45f6644c669ce27158228af96
David Moulton <dbmoulton@gmail.com>
2023-03-13 22:15:00 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(e.target)
+    buildToDo(e.target[0].value)
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.getElementById('tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
